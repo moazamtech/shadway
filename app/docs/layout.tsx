f3f8ae040b46213d8f99945008c717d3dee5441a
Moazam <moazamdevw@gmail.com>
2025-11-18 17:18:56 +0500

    header updated

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -13,16 +13,8 @@ export default function BlockLayout({
       <Header />
       <div className="flex flex-1 flex-col">
         <div className="relative mx-auto w-full max-w-(--breakpoint-xl) flex-1 border-dashed border-r border-l px-4 sm:px-8 overflow-hidden">
-          <BorderBeam borderWidth={1} reverse initialOffset={50}
-        delay={3}
-        className="from-transparent via-blue-500 to-transparent"
- 
-          />
-          <BorderBeam borderWidth={1} 
-                  className="from-transparent via-blue-500 to-transparent"
-
-          />
-
+          <BorderBeam borderWidth={1} reverse initialOffset={10} className="from-transparent via-blue-500 to-transparent"/>
+          <BorderBeam borderWidth={1} className="from-transparent via-blue-500 to-transparent"/>
           <div className="min-h-[calc(100%-2rem)] w-full pt-10 pb-20">
             {children}
           </div>
